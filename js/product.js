c4fd21cf87e6d214c95ca06448a067a96fcf39d5
Ivan <ivan.savkiv.igorovych@gmail.com>
2023-02-27 12:32:52 +0200

    Done popup cart

--- js/product.js
+++ js/product.js
@@ -1,3 +1,4 @@
+//for increase/decrease buttons
 const quantityInput = document.querySelector("#quantity");
 const increaseBtn = document.querySelector(".cart_increase");
 const decreaseBtn = document.querySelector(".cart_decrease");
@@ -14,4 +15,19 @@ decreaseBtn.addEventListener("click", function () {
   value = isNaN(value) ? 1 : value;
   value = value < 2 ? 1 : value - 1;
   quantityInput.value = value;
+});
+
+//for popup window
+const addToCartBtn = document.querySelector("#addToCartBtn");
+const cartPopup = document.querySelector("#cartPopup");
+
+addToCartBtn.addEventListener("click", function () {
+  addToCartBtn.innerHTML = "&#10003;";
+
+  cartPopup.classList.add("show");
+
+  setTimeout(function () {
+    cartPopup.classList.remove("show");
+    addToCartBtn.innerHTML = "Dodaj do koszyka";
+  }, 5000);
 });
